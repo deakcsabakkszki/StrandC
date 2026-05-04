61c0e6d49afb1f5ae1a557540eb766feadcec6d7
deakcs <deakcs@310-T>
2026-05-04 14:57:01 +0200

    8. feladat

--- Program.cs
+++ Program.cs
@@ -11,7 +11,14 @@
             }
             Console.WriteLine("7.feladat:");
             Console.WriteLine($"Fürdők száma: {list.Count}");
-            
+            //8.	Adja meg, mennyi a fürdőbelépők átlagos ára! Az eredmény egy tizedesre kerekítve írja ki!   
+            int összeg = 0;
+            foreach (var item in list)
+            {
+                összeg += item.Ár;
+            }
+            double átlag=összeg/(double)list.Count;
+            Console.WriteLine($"8. feladat\r\nA fürdőbelépők átlagos ára: {átlag:F1}\r\n");
         }
     }
 }
