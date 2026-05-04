12620bdb252b448b891a505fd74b907d6b9f33ef
deakcs <deakcs@310-T>
2026-05-04 14:36:34 +0200

    Függvények, 4. Feladat

---
+++ Furdo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrandC
+{
+    public class Furdo
+    {
+        public String Név { get; private set; }
+        public String Cím { get; private set; }
+        public int Ár { get; private set; }
+        public int Vízhőfok { get; private set; }
+
+
+        //Gyulai Várfürdő;5700 Gyula, Várfürdő u. 1.;6000;36
+        public Furdo(String sor)
+        {
+            string[] adatok=sor.Split(";");
+            Név=adatok[0];
+            Cím = adatok[1];
+            Ár=int.Parse(adatok[2]);
+            Vízhőfok=int.Parse(adatok[3]);
+        }
+
+        public String IRSZ()
+        {
+            //5700 Gyula, Várfürdő u. 1.
+            return Cím.Split(" ")[0];  
+        }
+        public String Telepules()
+        {
+            //5700 Gyula, Várfürdő u. 1.
+            return Cím.Split(" ")[1].Split(",")[0];//
+        }
+    }
+}
