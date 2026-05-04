1a86b054503e71d5d1dfb956a54251e16080d674
deakcs <deakcs@310-T>
2026-05-04 14:41:07 +0200

    5. Feladat

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-          
+            List<Furdo> list = new List<Furdo>();
+            string[] sorok = File.ReadAllLines("strandadatok.txt").Skip(1).ToArray();
+            foreach (string s in sorok) { 
+                list.Add(new Furdo(s));
+            }
+
         }
     }
 }
